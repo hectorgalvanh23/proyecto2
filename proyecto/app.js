1558132dbcaebfc6c00b37f0d600405b216e0311
hectorgalvanh23 <hectorgalvanh23@gmail.com>
2016-03-14 23:34:44 +0000

    Primeros views

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,5 +1,11 @@
 var express = require("express");
 var app = express();
+var bodyParser=require("body-parser");
+var log4js=require("log4js");
+
+
+app.use(bodyParser.json());// Application/JSON
+app.use(bodyParser.urlencoded({extends:true}));
 app.set("view engine","jade");
 
 app.get("/", function(req, res){
